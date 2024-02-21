8c7225c72011e4b2e2b29474480eadb95db52f8e
Firaol <firaoltolessa108@gmail.com>
2024-02-21 11:56:55 +0300

    Added header text

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,8 @@
 function header() {
     return (
-        <div class=" flex  flex-col text-center">
-            <div class="basis-auto w-10" >Food Menu</div>
-            <div class="flex flex-row bg-slate-600">
+        <div class="flex-col text-center items-center bg-green-900 ">
+            <div class="py-4 font-bold text-white text-9xl" >Food Menu</div>
+            <div class="flex bg-slate-600">
                 <div class="basis-1/4 bg-red-500 m-5 rounded-full  text-center">01</div>
                 <div class="basis-1/2  bg-yellow-200 m-7 rounded-full text-center">02</div>
                 <div class="basis-1  bg-green-600 m-7 rounded-full text-center">03</div>
